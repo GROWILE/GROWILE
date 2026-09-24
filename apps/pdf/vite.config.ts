e0fcfc5953c712505d3298a28680dffb9697b4d7
RanjithDEV-prog <ranjithselvakumar18@gmail.com>
2026-09-25 01:12:30 +0530

    vite configure checked

--- apps/pdf/vite.config.ts
+++ apps/pdf/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       // 👇 packages/ui-ku kulla irukkura src-a point panrom
